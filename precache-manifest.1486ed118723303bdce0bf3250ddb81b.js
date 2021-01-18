0b356ccaea2f4d874900b9c6becd694014218a4c
andrewroar <59775822+andrewroar@users.noreply.github.com>
2021-01-18 10:41:53 +0000

    Updates

---
+++ precache-manifest.1486ed118723303bdce0bf3250ddb81b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0daebb869738cd2d15986323a31aa82",
+    "url": "/ygo-react/index.html"
+  },
+  {
+    "revision": "84e5ada25499d60f1bee",
+    "url": "/ygo-react/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "659f9232c13ac2027595",
+    "url": "/ygo-react/static/css/main.257b5b30.chunk.css"
+  },
+  {
+    "revision": "84e5ada25499d60f1bee",
+    "url": "/ygo-react/static/js/2.c5db9773.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/ygo-react/static/js/2.c5db9773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659f9232c13ac2027595",
+    "url": "/ygo-react/static/js/main.df2cf9ae.chunk.js"
+  },
+  {
+    "revision": "1c6c7ce080bdb7c1c6af",
+    "url": "/ygo-react/static/js/runtime-main.ad3d85b7.js"
+  }
+]);
